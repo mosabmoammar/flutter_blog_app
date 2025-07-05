d6df30977b3f9171d65e947061bd650b653d61f3
Mosab N. R. AbuMoammar <mosab.abumoammar@gmail.com>
2025-07-05 13:23:31 +0300

    update .gitignore

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,5 +1,4 @@
 class AppSecrets {
-  static const supabaseUrl = 'https://jtixeoabepmbrhxevlem.supabase.co';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0aXhlb2FiZXBtYnJoeGV2bGVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxNDg2ODYsImV4cCI6MjA2NDcyNDY4Nn0.3Dae45lrgyWohfroiYHeLeC3QPCKxG05uOG1lEBDpI0';
+  static const supabaseUrl = 'YOUR_SUPABASE_URL';
+  static const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
 }
